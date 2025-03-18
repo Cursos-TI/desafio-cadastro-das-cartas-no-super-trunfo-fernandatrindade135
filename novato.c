3b630e9847b2f983da3c90eb7bbe24ddff15a7a0
fernandatrindade135 <fernanda.trindade12@gmail.com>
2025-03-18 23:27:21 +0000

    criando as declaraçoes

---
+++ novato.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+int main() {
+    //Declaração de variaveis  para a primeira carta
+    char Estado[15];
+    char Codigodacarta[4];
+    char Cidade[15];
+    int Populacao;
+    float area_em_km;
+    float PIB; 
+    int Pontosturisticos;
+
+    // Declaração de dados da primeira carta
+    printf("Desafio novato!\n");
+    printf("Carta 1\n");
+    printf("Digite o Estado:\n");
+    scanf("%s", Estado);
+    printf("Digite o codigo da carta:\n");
+    scanf("%s", Codigodacarta);
+    printf("Digite a cidade:\n");
+    scanf("%s", Cidade);
+    printf("Digite a populacao:\n");
+    scanf("%d", &Populacao);
+    printf("Digite area_em_km:\n");
+    scanf("%f", &area_em_km);
+    printf("Digite o PIB:\n ");
+    scanf("%f", &PIB);
+    printf("Digite os Pontos turisticos:\n");
+    scanf("%d", &Pontosturisticos);
+
+    //Declaração de variaveis  para a segunda carta
+    char Estado[30];
+    char Codigodacarta[3];
+    char Cidade[30];
+    int Populacao;
+    float area_em_km;
+    float PIB; 
+    int Pontosturisticos;
+
+    // Declaração de dados da primeira carta
+    printf("Segunda Carta !\n");
+    printf("Carta 2\n");
+    printf("Digite o Estado:\n");
+    scanf("%s", Estado);
+    printf("Digite o codigo da carta:\n");
+    scanf("%s", Codigodacarta);
+    printf("Digite a cidade:\n");
+    scanf("%s", Cidade);
+    printf("Digite a populacao:\n");
+    scanf("%d", &Populacao);
+    printf("Digite area_em_km:\n");
+    scanf("%f", &area_em_km);
+    printf("Digite o PIB:\n ");
+    scanf("%f", &PIB);
+    printf("Digite os Pontos turisticos:\n");
+    scanf("%d", &Pontosturisticos);
+
+    return 0;
+
+}
